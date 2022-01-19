b6fd1da1d4ab355d68ab27fbccdb65f015929188
Giorgi Vadachkoria <gigivadachkoria@gmail.com>
2022-01-19 15:57:39 +0400

    getting myjobs from database

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -2,10 +2,12 @@ import React from "react";
 import { MdDelete } from "react-icons/md";
 import { AiFillEdit } from "react-icons/ai";
 
-const JobCard = () => {
+const JobCard = ({ job }) => {
   return (
     <div className="flex justify-between items-center mt-12 bg-gray-200 py-4 px-4 rounded-md lg:w-[60%] mx-auto">
-      <h1 className="font-semibold">job card</h1>
+      <h1 className="font-semibold">
+        {job.title} - {job.company}
+      </h1>
       <div className="flex space-x-4">
         <AiFillEdit className="text-xl text-blue-600" />
         <MdDelete className="text-xl text-red-600" />
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import DashboardHeader from "../components/DashboardHeader";
 import JobCard from "../components/JobCard";
 
+import { useDispatch, useSelector } from "react-redux";
+import { getMyJobs } from "../actions/jobs";
+
 const Dashboard = () => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+  const { myjobs } = useSelector((state) => state.jobs);
+
+  useEffect(() => {
+    dispatch(getMyJobs(user?.token));
+  }, [dispatch, user?.token]);
+
   return (
     <div className="w-[90%] mx-auto mt-12">
       <DashboardHeader />
-      <JobCard />
+      {myjobs.length < 1 && (
+        <p className="mt-16 text-center font-semibold">No Jobs Found!</p>
+      )}
+      {myjobs.map((job) => (
+        <JobCard job={job} />
+      ))}
     </div>
   );
 };
