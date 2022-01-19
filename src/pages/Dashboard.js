8967d9f8f91743e1bb0eda36328af24ea4bc1093
Giorgi Vadachkoria <gigivadachkoria@gmail.com>
2022-01-19 14:18:38 +0400

    added logout logic on dashboard page and navigating to home  page

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { IoIosLogOut } from "react-icons/io";
 
 const Dashboard = () => {
+  const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    localStorage.removeItem("user");
+    dispatch({ type: "LOGOUT" });
+    navigate("/", { replace: true });
+  };
+
   return (
     <div>
-      <h1>dashboard</h1>
+      <h1
+        onClick={handleClick}
+        className="cursor-pointer font-semibold text-xl flex items-center"
+      >
+        {user?.email}{" "}
+        <IoIosLogOut className="ml-2 mt-1 text-blue-500 text-2xl" />
+      </h1>
     </div>
   );
 };
