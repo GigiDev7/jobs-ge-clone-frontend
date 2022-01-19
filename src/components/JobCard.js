2444fc446c72b5da02eda80153ace608c0994393
Giorgi Vadachkoria <gigivadachkoria@gmail.com>
2022-01-19 16:04:59 +0400

    minor styling change

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -4,7 +4,7 @@ import { AiFillEdit } from "react-icons/ai";
 
 const JobCard = ({ job }) => {
   return (
-    <div className="flex justify-between items-center mt-12 bg-gray-200 py-4 px-4 rounded-md lg:w-[60%] mx-auto">
+    <div className="flex justify-between items-center mt-12 bg-gray-200 hover:bg-gray-300 py-4 px-4 rounded-md lg:w-[60%] mx-auto">
       <h1 className="font-semibold">
         {job.title} - {job.company}
       </h1>
